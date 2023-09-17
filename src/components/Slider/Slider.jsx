adf80196c94805210f4bf2e777946b1ea445179c
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-18 01:25:22 +0400

    auto-play done

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,27 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { IconButton } from '../IconButton/IconButton';
 import './style.scss';
 
 const Slider = ({ children }) => {
     const [activeId, setActiveId] = useState(0)
 
-const prev = () => {
-    setActiveId(activeId => {
-        if (activeId > 0) {
-            return activeId - 1
-        }
-        return activeId
-    })
-}
+// перемотка слайдов только вручную
+// const prev = () => {
+//     setActiveId(activeId => {
+//         if (activeId > 0) {
+//             return activeId - 1
+//         }
+//         return activeId
+//     })
+// }
 
-const next = () => {
-    setActiveId((activeId) => {
-        if (activeId < children.length - 1) {
-        return activeId + 1;
-        }
-        return activeId;
-    });
-};
+// const next = () => {
+//     setActiveId((activeId) => {
+//         if (activeId < children.length - 1) {
+//         return activeId + 1;
+//         }
+//         return activeId;
+//     });
+// };
+
+// автопроигрывание
+const prev = () => {
+    if (activeId > 0) {
+        setActiveId(activeId - 1);
+    }
+    };
+
+    const next = () => {
+    if (activeId < children.length - 1) {
+        setActiveId(activeId + 1);
+    } else {
+        setActiveId(0); 
+    }
+    };
+
+    useEffect(() => {
+    const timer = setInterval(() => {
+        next(); 
+    }, 5000); 
+
+    return () => {
+        clearTimeout(timer); 
+    };
+}, [activeId]);
 
     return (
         <div className="slider-wrap">
