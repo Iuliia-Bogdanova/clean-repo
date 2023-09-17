6541e75c6107fbccf2cf9932d4603b9031a39c5c
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-17 14:50:31 +0400

    gallery actions doing

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,10 +1,22 @@
-import "./style.scss";
+import style from"./style.module.scss";
 
 const Gallery = () => {
-    return ( 
-        <>
-            <h1>галерея котиков</h1>
-        </>
+    return (
+        <main className={style.main}>
+            <div className="container">
+                <div className={style.box}>
+                    <h1 className="logo logo-title">
+                    отзывы питомцев
+                    <br />
+                    <span className="logo">Happy Cats</span>
+                    </h1>
+                    <div className={style.actions}>
+                        <div className={style.prev}></div>
+                        <div className={style.next}></div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
