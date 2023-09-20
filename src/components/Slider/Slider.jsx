26f1b32d02ad9ed2eda3990ab63418e60330e4f3
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-20 17:11:21 +0400

    indicators doing

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -40,41 +40,42 @@ const togglePlaying = () => {
     return (
         <div className="slider-wrap">
             <div className="slider-actions">
-                <IconButton
-                    direction="left"
-                    onClick={prev}
-                    disable={activeId === 0}
-                />
+            <IconButton
+                direction="left"
+                onClick={prev}
+                disable={activeId === 0}
+            />
 
-                <PlayButton
-                    active={isPlaying ? "active" : ""}
-                    disable={false}
-                    onClick={togglePlaying}
-                />
+            <PlayButton
+                active={isPlaying ? "active" : ""}
+                disable={false}
+                onClick={togglePlaying}
+            />
 
-                <IconButton
-                    direction="right"
-                    onClick={next}
-                    disable={activeId === data.length - 1}
-                />
+            <IconButton
+                direction="right"
+                onClick={next}
+                disable={activeId === data.length - 1}
+            />
             </div>
-            
             <div className="slider">
-                {data.map((slide, idx) => (
-                    <div
-                    key={idx}
-                    className={`slide${idx === activeId ? " active" : ""}`}
-                    >
-                    <div className="slide__info">
-                        <div className="slide__name">{slide.name}</div>
-                        <div className="slide__text">{slide.text}</div>
-                    </div>
-                    <img src={slide.img} alt={`Slide ${idx + 1}`} />
-                    </div>
-                ))}
+            {data.map((slide, idx) => (
+                <div
+                key={idx}
+                className={`slide${idx === activeId ? " active" : ""}`}
+                >
+                <div className="slide__info">
+                    <div className="slide__name">{slide.name}</div>
+                    <div className="slide__text">{slide.text}</div>
+                </div>
+                <img src={slide.img} alt={`Slide ${idx + 1}`} />
+                </div>
+            ))}
             </div>
-
             
+            <div className="slide-indicators">
+                <div className="indicator"></div>
+            </div>
 
         </div>
     );
