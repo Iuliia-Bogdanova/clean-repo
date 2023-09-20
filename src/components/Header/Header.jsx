40ce10f556c9ed5cc2a7a3f28694013dbc0667b4
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-20 19:22:12 +0400

    routing done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,37 +3,55 @@ import { NavLink } from "react-router-dom";
 import "./style.scss";
 
 const Header = () => {
+
+    const activeLink = "nav-list__link nav-list__link--active";
+    const normalLink = "nav-list__link";
+
     return (
-      <header className="header">
-        <nav className="nav">
-          <div className="container">
-            <div className="nav-row">
-              
-              <ul className="nav-list">
-                <li className="nav-list__item">
-                  <NavLink to="/" className="nav-list__link">
-                    о нас
-                  </NavLink>
-                </li>
+        <header className="header">
+            <nav className="nav">
+            <div className="container">
+                <div className="nav-row">
+                <ul className="nav-list">
+                    <li className="nav-list__item">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                        isActive ? activeLink : normalLink
+                        }
+                    >
+                        о нас
+                    </NavLink>
+                    </li>
 
-                <li className="nav-list__item">
-                  <NavLink to="/gallery" className="nav-list__link">
-                    галерея котиков
-                  </NavLink>
-                </li>
+                    <li className="nav-list__item">
+                    <NavLink
+                        to="/gallery"
+                        className={({ isActive }) =>
+                        isActive ? activeLink : normalLink
+                        }
+                    >
+                        галерея котиков
+                    </NavLink>
+                    </li>
 
-                <li className="nav-list__item">
-                  <NavLink to="/contacts" className="nav-list__link">
-                    контакты
-                  </NavLink>
-                </li>
-              </ul>
+                    <li className="nav-list__item">
+                    <NavLink
+                        to="/contacts"
+                        className={({ isActive }) =>
+                        isActive ? activeLink : normalLink
+                        }
+                    >
+                        контакты
+                    </NavLink>
+                    </li>
+                </ul>
 
-              <h2 className="logo">Happy Cats</h2>
+                <h2 className="logo">Happy Cats</h2>
+                </div>
             </div>
-          </div>
-        </nav>
-      </header>
+            </nav>
+        </header>
     );
 }
 
