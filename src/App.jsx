fb735090043b8959eff6a88643f80faa66ff1a92
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-20 18:59:34 +0400

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import "./styles/base.scss";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import About from "./pages/about/About";
-import Gallery from "./pages/gallery/Gallery";
-import Contacts from "./pages/contacts/Contacts";
+import Home from "./pages/Home/Home";
+import Gallery from "./pages/Gallery/Gallery";
+import Contacts from "./pages/Contacts/Contacts";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* <About /> */}
-      <Gallery />
-      {/* <Contacts /> */}
-      <Footer />
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,39 @@
+import { NavLink } from "react-router-dom";
+
 import "./style.scss";
 
 const Header = () => {
     return (
-        <header className="header">
-            <nav className="nav">
-            <div className="container">
-                <div className="nav-row">
-                <ul className="nav-list">
-                    <li className="nav-list__item">
-                    <a href="" className="nav-list__link nav-list__link">
-                        о нас
-                    </a>
-                    </li>
-                    <li className="nav-list__item">
-                    <a href="" className="nav-list__link--active">
-                        галерея котиков
-                    </a>
-                    </li>
-                    <li className="nav-list__item">
-                    <a href="" className="nav-list__link">
-                        контакты
-                    </a>
-                    </li>
-                </ul>
-                <h2 className="logo">Happy Cats</h2>
-                </div>
+      <header className="header">
+        <nav className="nav">
+          <div className="container">
+            <div className="nav-row">
+              
+              <ul className="nav-list">
+                <li className="nav-list__item">
+                  <NavLink to="/" className="nav-list__link">
+                    о нас
+                  </NavLink>
+                </li>
+
+                <li className="nav-list__item">
+                  <NavLink to="/gallery" className="nav-list__link">
+                    галерея котиков
+                  </NavLink>
+                </li>
+
+                <li className="nav-list__item">
+                  <NavLink to="/contacts" className="nav-list__link">
+                    контакты
+                  </NavLink>
+                </li>
+              </ul>
+
+              <h2 className="logo">Happy Cats</h2>
             </div>
-            </nav>
-        </header>
+          </div>
+        </nav>
+      </header>
     );
 }
 
