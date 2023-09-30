2bb852446088ae78d6905e55a1db0fa81d04a81f
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-10-01 00:08:44 +0400

    preview doing

--- src/Gallery/PrewiewGallery/index.tsx
+++ src/Gallery/PrewiewGallery/index.tsx
@@ -1,4 +1,8 @@
+import cl from 'classnames';
+
 import { Photo, CommonClassProps } from "../types";
+
+import style from './index.module.scss';
 
 interface PreviewGalleryProps extends CommonClassProps {
     activePhotoIndex: number;
@@ -8,6 +12,26 @@ interface PreviewGalleryProps extends CommonClassProps {
 export const PreviewGallery: React.FC<PreviewGalleryProps> = ({
     activePhotoIndex,
     photos,
+    className
 }) => (
-    <div>Preview Gallery</div>
-)
+    <div className={cl(style.previewGallery, className)}>
+        <ul className={cl(style.previewGalleryTrack)}
+        >
+            {photos.map((photo) => (
+                <li
+                    key={photo.id}
+                    className={style.previewGalleryPreview}
+                >
+                    <img
+                        src={photo.preview}
+                        alt={photo.description}
+                        className={style.previewGalleryImage}
+                    />
+                </li>
+            ))}
+        </ul>
+        <div className={style.previewGalleryCover}>
+            {activePhotoIndex + 1} / {photos.length}
+        </div>
+    </div>
+);
